20697bcbca68425fef8a0c89db9024d7b85fb5a7
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-09-15 22:59:46 +0900

    install ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    project: './tsconfig.json',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint', 'react'],
+  root: true,
+  rules: {}
+};
