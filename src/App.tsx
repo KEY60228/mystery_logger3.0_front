5c04b2556e686427101279fab54794f3e0fbb450
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-10-02 17:03:30 +0900

    add React Router (& CssBaseline)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React, { FC } from 'react'
-import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { CssBaseline } from '@material-ui/core'
 
 const App: FC = () => {
   return (
     <>
-      <h1>App</h1>
+      <CssBaseline />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact><h1>App</h1></Route>
+        </Switch>
+      </BrowserRouter>
     </>
   )
 }
