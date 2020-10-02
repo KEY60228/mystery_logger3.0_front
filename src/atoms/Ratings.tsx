4e02994d757437d4fffaf8ca854346036944e627
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-10-02 17:31:00 +0900

    add atoms/Ratings

---
+++ src/atoms/Ratings.tsx
@@ -0,0 +1,49 @@
+import React, { FC  } from 'react'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+import { Grid } from '@material-ui/core'
+import { Rating } from '@material-ui/lab'
+import { Typography } from '@material-ui/core'
+
+interface Props {
+  number: number
+  size?: 'large'
+  | 'medium'
+  | 'small'
+  className?: ClassProps
+}
+
+interface ClassProps {
+  width?: string
+  fontSize?: string
+}
+
+const useStyles = makeStyles((theme: Theme) => 
+  createStyles({
+    root: (className: ClassProps) => ({
+      width: className.width
+    }),
+    text: (className: ClassProps) => ({
+      fontSize: className.fontSize
+    })
+  })
+)
+
+export const Ratings: FC<Props> =({
+  number, size, className
+}) => {
+  const classes = useStyles(className)
+  
+  return (
+    <Grid container justify='space-around' alignItems='center' className={classes.root}>
+      <Rating
+        value={number == 0 ? 0 : parseFloat(number.toFixed(1))}
+        precision={0.1}
+        readOnly
+        size={size}
+      />
+      <Typography variant='caption' className={classes.text}>
+        {number == 0 ? 0 : number.toFixed(1)}
+      </Typography>
+    </Grid>
+  )
+}
