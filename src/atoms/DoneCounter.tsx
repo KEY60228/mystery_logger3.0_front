2d978f4a0a750fec77dcf7bfb87c9c5367b133bb
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-10-02 17:20:58 +0900

    add atoms/DoneCounter

---
+++ src/atoms/DoneCounter.tsx
@@ -0,0 +1,51 @@
+import React, { FC } from 'react'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+import { Grid } from '@material-ui/core'
+import { Typography } from '@material-ui/core'
+import DirectionsRunIcon from '@material-ui/icons/DirectionsRun'
+
+interface Props {
+  number: number
+  iconSize?: 'default'
+  | 'inherit'
+  | 'large'
+  | 'small'
+  className?: ClassProps
+}
+
+interface ClassProps {
+  width?: string
+  fontSize?: string
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: (className: ClassProps) => ({
+      width: className.width,
+    }),
+    text: (className: ClassProps) => ({
+      fontSize: className.fontSize,
+    }),
+  })
+)
+
+export const DoneCounter: FC<Props> = ({
+  number, iconSize, className
+}) => {
+  const classes = useStyles(className)
+
+  return (
+    <Grid 
+      container
+      direction='column'
+      alignItems='center'
+      justify='center'
+      className={classes.root}
+    >
+      <DirectionsRunIcon color='primary' fontSize={iconSize} />
+      <Typography variant='caption'  className={classes.text}>
+        {number}
+      </Typography>
+    </Grid>
+  )
+}
