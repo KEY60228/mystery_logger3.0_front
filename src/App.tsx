536bcdc25b3763db2c09620b3f93f29f74736d70
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-10-04 23:23:23 +0900

    call Header, BottomNav, and TopPage from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,20 @@ import React, { FC } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { CssBaseline } from '@material-ui/core'
 
+import { Header } from './organisms/Header'
+import { TopPage } from './pages/TopPage'
+import { BottomNav } from './organisms/BottomNav'
+
 const App: FC = () => {
   return (
     <>
       <CssBaseline />
       <BrowserRouter>
+        <Header />
         <Switch>
-          <Route path="/" exact><h1>App</h1></Route>
+          <Route path="/" exact children={<TopPage />} />
         </Switch>
+        <BottomNav />
       </BrowserRouter>
     </>
   )
