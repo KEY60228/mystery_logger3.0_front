857961745410bcb2d01b7c834b252497ae144ab7
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-10-07 09:53:16 +0900

    add pages/PreRegister

---
+++ src/pages/PreRegister.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+import { PreRegister as PreRegisterTemp } from '../templates/PreRegister'
+
+export const PreRegister: FC = () => {
+  return (
+    <PreRegisterTemp />
+  )
+}
