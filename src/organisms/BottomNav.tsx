1457217674d777547ab1a09dce9c255d029c9f24
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-10-08 20:02:03 +0900

    modify BottomNav (switch by logged in or not)

--- src/organisms/BottomNav.tsx
+++ src/organisms/BottomNav.tsx
@@ -1,12 +1,18 @@
 import React, { FC, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core'
 import HomeIcon from '@material-ui/icons/Home'
+import HistoryIcon from '@material-ui/icons/History'
 import SearchIcon from '@material-ui/icons/Search'
 import TrendingUpIcon from '@material-ui/icons/TrendingUp'
 import HowToRegIcon from '@material-ui/icons/HowToReg'
+import NotificationsIcon from '@material-ui/icons/Notifications'
 import LockOpenIcon from '@material-ui/icons/LockOpen'
+import PersonIcon from '@material-ui/icons/Person'
+
+import { RootState } from '../stores/index'
 
 const useStyles = makeStyles((theme: Theme) => 
   createStyles({
@@ -25,6 +31,10 @@ export const BottomNav: FC = (
   const classes = useStyles()
   const [value, setValue] = useState<number>(0)
 
+  const isLogin = useSelector((state: RootState) => !! state.auth.user)
+  const userName = useSelector((state: RootState) => state.auth.user ? state.auth.user.name : '')
+  const userId = useSelector((state: RootState) => state.auth.user ? state.auth.user.account_id : '')
+
   return (
     <BottomNavigation
       value={value}
@@ -32,12 +42,22 @@ export const BottomNav: FC = (
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction
-        component={Link}
-        to={'/'}
-        label='Top'
-        icon={<HomeIcon />}
-      />
+      { !isLogin &&
+        <BottomNavigationAction
+          component={Link}
+          to={'/'}
+          label='Top'
+          icon={<HomeIcon />}
+        />
+      }
+      { isLogin &&
+        <BottomNavigationAction
+          component={Link}
+          to={'/timeline'}
+          label='Timeline'
+          icon={<HistoryIcon />}
+        />
+      }
       <BottomNavigationAction
         component={Link}
         to={'/search'} 
@@ -50,18 +70,38 @@ export const BottomNav: FC = (
         label='Trend' 
         icon={<TrendingUpIcon />} 
       />
-      <BottomNavigationAction
-        component={Link}
-        to={'/preregister'} 
-        label='Sign up' 
-        icon={<HowToRegIcon />} 
-      />
-      <BottomNavigationAction
-        component={Link}
-        to={`/login`} 
-        label='Login' 
-        icon={<LockOpenIcon />} 
-      />
+      { !isLogin &&
+        <BottomNavigationAction
+          component={Link}
+          to={'/preregister'} 
+          label='Sign up' 
+          icon={<HowToRegIcon />} 
+        />
+      }
+      { isLogin &&
+        <BottomNavigationAction
+          component={Link}
+          to={'/notifications'} 
+          label='Notifications' 
+          icon={<NotificationsIcon />} 
+        />
+      }
+      { !isLogin &&
+        <BottomNavigationAction
+          component={Link}
+          to={`/login`} 
+          label='Login' 
+          icon={<LockOpenIcon />} 
+        />
+      }
+      { isLogin &&
+        <BottomNavigationAction
+          component={Link}
+          to={`/users/${userId}`} 
+          label={`${userName}`} 
+          icon={<PersonIcon />} 
+        />
+      }
     </BottomNavigation>
   )
 }
