682659ed56d1af0251ab4a26849eb19d185c94a5
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-10-12 22:57:55 +0900

    add organisms/ListedProducts

---
+++ src/organisms/ListedProducts.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from 'react'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+import { Grid } from '@material-ui/core'
+
+import { Product } from '../@types'
+import { ProductCard } from './ProductCard'
+
+interface Props {
+  products: Product[]|null
+  className?: ClassProps
+}
+
+interface ClassProps {
+  width?: string
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '&::after': {
+        content: '',
+        flex: 'auto',
+      }
+    }
+  })
+)
+
+export const ListedProducts: FC<Props> = ({
+  products, className
+}) => {
+  const classes = useStyles(className)
+
+  return (
+    <Grid container justify='space-between' className={classes.root}>
+      { products &&
+        products.map((product: Product) => (
+          <ProductCard key={product.id} product={product} />
+        ))
+      }
+    </Grid>
+  )
+}
