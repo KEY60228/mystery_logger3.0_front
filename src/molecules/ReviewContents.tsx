76d34316444722ae8e793eec70db0f79ee8f5a11
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-10-18 21:59:00 +0900

    modify molecules/ReviewContents

--- src/molecules/ReviewContents.tsx
+++ src/molecules/ReviewContents.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react'
 import  { Link } from 'react-router-dom'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 import { Box, Grid, CardActionArea, Typography } from '@material-ui/core'
+import NavigateNextIcon from '@material-ui/icons/NavigateNext'
 
 import { Review, Product } from '../@types'
 import { Ratings } from '../atoms/Ratings'
@@ -52,20 +53,25 @@ export const ReviewContents: FC<Props> = ({
         <Box>
           { cardActionArea &&
             <CardActionArea component={Link} to={`/reviews/${review.id}`} className={classes.actionArea}>
-              { (productTitle && review.product) &&
-                <Typography>{ review.product.name }</Typography>
-              }
-              <Grid container>
-                <Ratings number={review.rating || 0} size='small' justify='flex-start' className={{ marginLeft: '4px' }} />
-                <Typography variant='body2' className={classes.remarks}>参加日: {review.joined_at || '-'} </Typography>
-                { review.result === 1 &&
-                  <Typography variant='body2' className={classes.remarks}>脱出成功！</Typography>
-                }
-                { review.result === 2 &&
-                  <Typography variant='body2' className={classes.remarks}>脱出失敗…</Typography>
-                }
+              <Grid container justify='space-between' wrap='nowrap' alignItems='center'>
+                <Grid direction='column'>
+                  { (productTitle && review.product) &&
+                    <Typography>{ review.product.name }</Typography>
+                  }
+                  <Ratings number={review.rating || 0} size='small' justify='flex-start' className={{ marginLeft: '4px' }} />
+                  <Grid container>
+                    <Typography variant='body2' className={classes.remarks}>参加日: {review.joined_at || '-'} </Typography>
+                    { review.result === 1 &&
+                      <Typography variant='body2' className={classes.remarks}>脱出成功！</Typography>
+                    }
+                    { review.result === 2 &&
+                      <Typography variant='body2' className={classes.remarks}>脱出失敗…</Typography>
+                    }
+                  </Grid>
+                  <Typography variant='body2'>{review.contents}</Typography>
+                </Grid>
+                <NavigateNextIcon />
               </Grid>
-              <Typography variant='body2'>{review.contents}</Typography>
             </CardActionArea>
           }
           { !cardActionArea &&
