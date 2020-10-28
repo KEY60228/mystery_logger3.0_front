23f3ac888a0e98ad75e0ea02070153eb86ee5c49
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-10-28 22:58:25 +0900

    modify molecules/ReviewerProfile

--- src/molecules/ReviewerProfile.tsx
+++ src/molecules/ReviewerProfile.tsx
@@ -1,12 +1,27 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 import { Grid, Box, CardMedia, CardActionArea, Typography, Button } from '@material-ui/core'
 
-import { User } from '../@types'
+import { Review, User, Product } from '../@types'
+import { RootState } from '../stores/index'
+import { ReviewerMenu } from './ReviewerMenu'
+
+interface ReviewDetail extends Review {
+  user?: User
+  product?: Product
+}
 
 interface Props {
-  user: User
+  review: ReviewDetail
+  setOpen: (value: boolean) => void
+  setRating: (value: number) => void
+  setResult: (value: number) => void
+  setJoined_at: (value: string|null) => void
+  setContents: (value: string|null) => void
+  setIsEdit: (value: boolean) => void
+  setReviewId: (value: number) => void
   className?: ClassProps
 }
 
@@ -49,31 +64,46 @@ const useStyles = makeStyles((theme: Theme) =>
 )
 
 export const ReviewerProfile: FC<Props> = ({
-  user, className
+  review, setOpen, setRating, setResult, setJoined_at, setContents, setIsEdit, setReviewId, className
 }) => {
   const classes = useStyles(className)
+  const currentUser = useSelector((state: RootState) => state.auth.user)
 
   return (
     <Grid container wrap='nowrap' alignItems='center'>
-      <CardActionArea component={Link} to={`/users/${user.account_id}`} className={classes.imgArea}>
+      <CardActionArea component={Link} to={`/users/${review.user?.account_id}`} className={classes.imgArea}>
         <CardMedia
-          image={`/user_img/${user.image_name}`}
+          image={`/user_img/${review.user?.image_name}`}
           className={classes.img}
         />
       </CardActionArea>
       <Box className={classes.nameArea}>
         <Grid container direction='column'>
-          <Typography variant='body1' component={Link} to={`/users/${user.account_id}`} className={classes.userName}>
-            { user.name }
+          <Typography variant='body1' component={Link} to={`/users/${review.user?.account_id}`} className={classes.userName}>
+            { review.user?.name }
           </Typography>
-          <Typography variant='body1' component={Link} to={`/users/${user.account_id}`} className={classes.userId}>
-            @{ user.account_id}
+          <Typography variant='body1' component={Link} to={`/users/${review.user?.account_id}`} className={classes.userId}>
+            @{ review.user?.account_id}
           </Typography>
         </Grid>
       </Box>
-      <Button variant='contained' color='primary'>
-        <Typography variant='button'>フォロー</Typography>
-      </Button>
+      { (currentUser && review.user?.account_id !== currentUser.account_id) &&
+        <Button variant='contained' color='primary'>
+          <Typography variant='button'>フォロー</Typography>
+        </Button>
+      }
+      { (currentUser && review.user?.account_id === currentUser.account_id) &&
+        <ReviewerMenu
+          review={review}
+          setOpen={setOpen}
+          setRating={setRating}
+          setResult={setResult}
+          setJoined_at={setJoined_at}
+          setContents={setContents}
+          setIsEdit={setIsEdit}
+          setReviewId={setReviewId}
+        />
+      }
     </Grid>
   )
 }
