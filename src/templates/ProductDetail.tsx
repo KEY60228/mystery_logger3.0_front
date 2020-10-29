869663f32f93152bc855558b631fc46f17e695f1
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-10-29 14:17:14 +0900

    modify templates/ProductDetail

--- src/templates/ProductDetail.tsx
+++ src/templates/ProductDetail.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC } from 'react'
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 
 import { ProductDetail as ProductDetailInterface } from '../@types'
@@ -9,6 +9,12 @@ import { TempSpace } from '../organisms/TempSpace'
 interface Props {
   product: ProductDetailInterface
   setOpen: (value: boolean) => void
+  setRating: (value: number) => void
+  setResult: (value: number) => void
+  setJoined_at: (value: string|null) => void
+  setContents: (value: string|null) => void
+  setIsEdit: (value: boolean) => void
+  setReviewId: (value: number) => void
 }
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -20,14 +26,23 @@ const useStyles = makeStyles((theme: Theme) =>
 )
 
 export const ProductDetail: FC<Props> = ({
-  product, setOpen
+  product, setOpen, setRating, setResult, setJoined_at, setContents, setIsEdit, setReviewId
 }) => {
   const classes = useStyles()
 
   return (
     <>
       <ProductContents product={product} setModalOpen={setOpen} />
-      <ProductReviews product={product} />
+      <ProductReviews
+        product={product}
+        setOpen={setOpen}
+        setRating={setRating}
+        setResult={setResult}
+        setJoined_at={setJoined_at}
+        setContents={setContents}
+        setIsEdit={setIsEdit}
+        setReviewId={setReviewId}
+      />
       <TempSpace text='Ad Space' className={{height: '320px', margin:'12px auto 60px'}} />
     </>
   )
