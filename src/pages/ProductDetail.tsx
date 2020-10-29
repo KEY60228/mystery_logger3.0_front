15438a6024958f55dfdbed09ae2531b1c77f992c
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-10-29 19:24:49 +0900

    modify pages/ProductDetail

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,9 +1,9 @@
 import React, { FC, useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
-// import { useSelector } from 'react-redux'
-// import { RootState } from '../stores/index'
+import { useSelector } from 'react-redux'
 
+import { RootState } from '../stores/index'
 import { ProductDetail as ProductDetailInterface } from '../@types'
 import { ProductDetail as ProductDetailTemp } from '../templates/ProductDetail'
 import { ReviewForm } from '../organisms/ReviewForm'
@@ -33,14 +33,14 @@ export const ProductDetail: FC = () => {
   const [result, setResult] = useState<number>(0)
   const [joined_at, setJoined_at] = useState<string|null>('')
   const [contents, setContents] = useState<string|null>('')
-  // const user = useSelector((state: RootState) => state.auth.user)
+  const user = useSelector((state: RootState) => state.auth.user)
   const post = async() => {
     const data = {
       rating: rating,
       result: result,
       joined_at: joined_at,
       contents: contents,
-      user_id: 1, // 仮
+      user_id: user?.id, // 仮
       product_id: product?.id, // 仮
       clear_time: null,
     }
@@ -63,6 +63,38 @@ export const ProductDetail: FC = () => {
     )
   }
 
+  const [isEdit, setIsEdit] = useState<boolean>(false)
+  const [reviewId, setReviewId] = useState<number>(0)
+  const edit = async() => {
+    const data = {
+      rating: rating,
+      result: result,
+      joined_at: joined_at,
+      contents: contents,
+      user_id: user?.id, // 仮
+      product_id: product?.id, // 仮
+      clear_time: null,
+    }
+
+    await axios.put(
+      `https://localhost:1443/v1/reviews/${reviewId}`,
+      data
+    ).then((response) => {
+      if (response.status === 201) {
+        console.log(response.data)
+      }
+      setOpen(false)
+      getProduct()
+      setRating(0)
+      setResult(0)
+      setJoined_at('')
+      setContents('')
+      setIsEdit(false)
+    }).catch((error) =>
+      console.log(error)
+    )
+  }
+
   useEffect(() => {
     getProduct()
   }, [])
@@ -71,7 +103,16 @@ export const ProductDetail: FC = () => {
     <>
       { product &&
         <>
-          <ProductDetailTemp product={product} setOpen={setOpen} />
+          <ProductDetailTemp
+            product={product}
+            setOpen={setOpen}
+            setRating={setRating}
+            setResult={setResult}
+            setJoined_at={setJoined_at}
+            setContents={setContents}
+            setIsEdit={setIsEdit}
+            setReviewId={setReviewId}
+          />
           <ReviewForm
             open={open}
             setOpen={setOpen}
@@ -84,6 +125,8 @@ export const ProductDetail: FC = () => {
             contents={contents}
             setContents={setContents}
             post={post}
+            edit={edit}
+            isEdit={isEdit}
             product={product}
           />
         </>
