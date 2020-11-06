3c85df503ba59a821963215001a5042cc0948645
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-11-06 12:56:58 +0900

    run asyncGetCurrentUser when the first access is occured

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 import { CssBaseline } from '@material-ui/core'
 
 import { Header } from './components/organisms/Header'
@@ -12,8 +13,15 @@ import { UserDetail } from './containers/UserDetail'
 import { ProductDetail } from './containers/ProductDetail'
 import { ReviewDetail } from './containers/ReviewDetail'
 import { Timeline } from './containers/Timeline'
+import { asyncGetCurrentUser } from './ajax/auth'
 
 const App: FC = () => {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(asyncGetCurrentUser())
+    }, [])
+
     return (
         <>
             <CssBaseline />
