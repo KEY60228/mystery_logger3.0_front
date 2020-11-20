b58660ffc89801eceebd8186fa3ef0e270677bbf
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-11-20 19:04:44 +0900

    add containers/Search

---
+++ src/containers/Search.tsx
@@ -0,0 +1,33 @@
+import React, { FC, useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { ProductDetail } from '../@types'
+import { asyncGetProducts } from '../ajax/product'
+import { Search as SearchTemp } from '../components/templates/Search'
+
+export const Search: FC = () => {
+    const dispatch = useDispatch()
+
+    const [products, setProducts] = useState<ProductDetail[] | null>(null)
+
+    const getProducts = () => {
+        dispatch(asyncGetProducts(setProducts))
+    }
+
+    useEffect(() => {
+        getProducts()
+    }, [])
+
+    return (
+        <>
+            { products &&
+                <SearchTemp
+                    products={products}
+                />
+            }
+            { !products &&
+                <div>loading</div>
+            }
+        </>
+    )
+}
