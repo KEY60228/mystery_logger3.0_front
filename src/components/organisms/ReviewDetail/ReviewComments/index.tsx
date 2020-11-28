7ba816182e51aa503b0171ec4e57f39c8a6959b1
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-11-28 12:53:22 +0900

    add organisms/ReviewDetail/ReviewComments/

---
+++ src/components/organisms/ReviewDetail/ReviewComments/index.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from 'react'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+
+import { CommentDetail } from '../../../../@types'
+import { CommentCard } from './CommentCard'
+
+interface Props {
+    comments: CommentDetail[]
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+
+        }
+    })
+)
+
+export const ReviewComments: FC<Props> = ({
+    comments
+}) => {
+    const classes = useStyles()
+
+    return (
+        <>
+            { comments.map((comment: CommentDetail) =>
+                <CommentCard comment={comment} />
+            )}
+        </>
+    )
+}
