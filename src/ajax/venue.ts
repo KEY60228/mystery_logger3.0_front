9d18695097046cbbf6a766fc1a3532f2b7914d0a
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-12-02 21:44:41 +0900

    add asyncGetVenue function

---
+++ src/ajax/venue.ts
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import { VenueDetail } from '../@types'
+
+// Ajaxリクエストであることを示すヘッダーを付与する
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+// Cookieの送信を許可する
+axios.defaults.withCredentials = true
+
+// デフォルトURLの設定
+axios.defaults.baseURL = 'https://localhost:1443'
+
+// エラーレスポンスが返って来た場合、レスポンスオブジェクトを返す
+axios.interceptors.response.use(
+    response => response,
+    error => error.response || error,
+)
+
+export const asyncGetVenue = (
+    venue_id: string,
+    setVenue: (value: VenueDetail) => void
+) => {
+    return async (dispatch: any) => {
+        const response = await axios.get(`/v1/venues/${venue_id}`)
+
+        if (response.status === 200) {
+            setVenue(response.data)
+        }
+
+        if (response.status === 422) {
+            // エラーハンドリング
+        }
+    }
+}
+
