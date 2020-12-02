8e4660262877f3ed18a422478bb0fee52a62bca2
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-12-02 21:44:11 +0900

    add templates/VenueDetail

---
+++ src/components/templates/VenueDetail.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react'
+
+import { VenueDetail as VenueDetailInterface } from '../../@types'
+import { VenueContents } from '../organisms/VenueDetail/VenueContents'
+import { VenuePerformances } from '../organisms/VenueDetail/VenuePerformances'
+import { SimpleMap } from '../organisms/VenueDetail/GoogleMaps'
+import { TempSpace } from '../molecules/TempSpace'
+
+interface Props {
+    venue: VenueDetailInterface
+}
+
+export const VenueDetail: FC<Props> = props => {
+    return (
+        <>
+            <VenueContents venue={props.venue} />
+            <SimpleMap venue={props.venue} />
+            <VenuePerformances performances={props.venue.performances} />
+            <TempSpace
+                text="Ad Space"
+                className={{ height: '320px', margin: '12px auto 60px' }}
+            />
+        </>
+    )
+}
