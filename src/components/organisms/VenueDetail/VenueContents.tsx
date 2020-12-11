d5e70e404969829118135ddc58fb7da5974289c4
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-12-11 17:09:22 +0900

    modify VenueDetail

--- src/components/organisms/VenueDetail/VenueContents.tsx
+++ src/components/organisms/VenueDetail/VenueContents.tsx
@@ -36,25 +36,29 @@ export const VenueContents: FC<Props> = props => {
     return (
         <Card className={classes.root}>
             <Typography className={classes.name}>
-                {/* {props.venue.name} */}
-                アジトオブスクラップ東新宿GUNKAN
+                {props.venue.name}
+                {/* アジトオブスクラップ東新宿GUNKAN */}
             </Typography>
             <Grid container justify='flex-end'>
                 <Typography className={classes.organizer}>
-                    {/* {props.venue.organizer.name} */}
-                    by 株式会社SCRUP
+                    {props.venue.organizer.service_name}
+                    {/* by 株式会社SCRUP */}
                 </Typography>
             </Grid>
             <Typography className={classes.address}>
-                {/* {props.venue.address} */}
-                〒169-0072
+                〒{props.venue.zipcode}
+                {/* 〒169-0072 */}
             </Typography>
             <Typography className={classes.address}>
-                東京都新宿区大久保1-1-10 GUNKAN東新宿 302
+                {props.venue.addr_prefecture}
+                {props.venue.addr_city}
+                {props.venue.addr_block}
+                {props.venue.addr_building}
+                {/* 東京都新宿区大久保1-1-10 GUNKAN東新宿 302 */}
             </Typography>
             <Typography className={classes.tel}>
-                {/* {props.venue.tel} */}
-                TEL: 03-6233-9868
+                {props.venue.tel}
+                {/* TEL: 03-6233-9868 */}
             </Typography>
         </Card>
     )
