61b4f870386c3626be8da58ea8762981d9056e9d
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-12-29 21:11:35 +0900

    add minimization option

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,6 +2,8 @@
 const fs = require('fs')
 // Dotenvモジュールの読み込み
 const Dotenv = require('dotenv-webpack')
+// minifization module
+const TerserPlugin = require('terser-webpack-plugin')
 // webpack-merge
 const { merge } = require('webpack-merge')
 // 共通設定
@@ -11,8 +13,19 @@ module.exports = merge(common, {
     // @ts-ignore
     // 開発モード
     mode: 'production',
+    // minifization
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+                output: {
+                    comments: false,
+                }
+            }
+        })],
+    },
     // dotenv設定
     plugins: [
         new Dotenv({path: './.env.prod'})
-    ]
+    ],
 })
