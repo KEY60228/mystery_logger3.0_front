002c3d9aae5c73591a5d0059b1783c49cb351fd1
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-05-28 22:48:54 +0900

    improve NotFound page #21

--- src/handlers/ErrorHandler/NotFoundPage.tsx
+++ src/handlers/ErrorHandler/NotFoundPage.tsx
@@ -1,18 +1,46 @@
 import React, { FC } from 'react'
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
-import { Card, Typography } from '@material-ui/core'
+import { useHistory } from 'react-router-dom'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
+import { Box, Button } from '@material-ui/core'
 
-const useStyles = makeStyles((theme: Theme) =>
+import { Footer } from '../../reusable/Footer'
+import { headerHeight, footerHeight } from '../../util'
+
+const useStyles = makeStyles(theme =>
     createStyles({
         root: {
             margin: '8px',
-            padding: '8px',
+            minHeight: `calc(100vh - ${headerHeight} - ${footerHeight} - 16px)`,
+        },
+        text: {
+            margin: '0',
+            fontSize: '14px',
+            lineHeight: '24px',
+        },
+        link: {
+            color: theme.palette.primary.main,
+            textDecoration: 'underline',
+        },
+        button: {
+            display: 'block',
+            margin: '16px auto',
         },
     }),
 )
 
 export const NotFoundPage: FC = () => {
     const classes = useStyles()
+    const history = useHistory()
 
-    return <Card className={classes.root}>ページが存在しません</Card>
+    return (
+        <>
+            <Box className={classes.root}>
+                <p className={classes.text} data-testid='failed'>ページが見つかりません</p>
+                <Button variant='contained' color='primary' onClick={() => history.push('/')} className={classes.button}>
+                    トップページに戻る
+                </Button>
+            </Box>
+            <Footer />
+        </>
+    )
 }
