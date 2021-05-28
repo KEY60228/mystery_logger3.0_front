4d5cf882ef2ece4245aad9c1f1ebf47f6b17eb59
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-05-28 19:55:30 +0900

    make header&footer's height into const #21

--- src/reusable/Footer.tsx
+++ src/reusable/Footer.tsx
@@ -3,16 +3,15 @@ import { Link } from 'react-router-dom'
 import { makeStyles, createStyles } from '@material-ui/core/styles'
 import { Box, Grid } from '@material-ui/core'
 
+import { footerHeight } from '../util'
+
 const useStyles = makeStyles(() => 
     createStyles({
         root: {
             backgroundColor: '#0A0A0A',
-            height: '240px',
+            height: footerHeight,
             marginTop: '40px',
             paddingTop: '40px',
-        },
-        guide: {
-            // marginBottom: '16px',
         },
         guideText: {
             color: '#FFAC00',
@@ -44,11 +43,11 @@ export const Footer: FC = () => {
 
     return (
         <Box className={classes.root}>
-            <Grid container justify="center" alignItems="center" className={classes.guide}>
+            <Grid container justify="center" alignItems="center">
                 <Link to="/kiyaku" className={classes.guideText}>利用規約</Link>
                 <Link to="/guideline" className={classes.guideText}>コミュニティガイドライン</Link>
             </Grid>
-            <Grid container justify="center" alignItems="center" className={classes.guide}>
+            <Grid container justify="center" alignItems="center">
                 <Link to="/policy" className={classes.guideText}>プライバシーポリシー</Link>
                 <Link to="/about" className={classes.guideText}>なぞログについて</Link>
                 <Link to="/contact" className={classes.guideText}>お問い合わせ</Link>
