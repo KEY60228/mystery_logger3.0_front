66064a7b27043d8826c9e678a005d0c8e9258b07
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2021-05-28 22:49:05 +0900

    improve SystemError page #21

--- src/handlers/ErrorHandler/SystemErrorPage.tsx
+++ src/handlers/ErrorHandler/SystemErrorPage.tsx
@@ -1,12 +1,28 @@
 import React, { FC } from 'react'
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
-import { Card } from '@material-ui/core'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
+import { Box } from '@material-ui/core'
 
-const useStyles = makeStyles((theme: Theme) =>
+import { Footer } from '../../reusable/Footer'
+import { headerHeight, footerHeight } from '../../util'
+
+const useStyles = makeStyles(theme =>
     createStyles({
         root: {
             margin: '8px',
-            padding: '8px',
+            minHeight: `calc(100vh - ${headerHeight} - ${footerHeight} - 16px)`,
+        },
+        text: {
+            margin: '0',
+            fontSize: '14px',
+            lineHeight: '24px',
+        },
+        link: {
+            color: theme.palette.primary.main,
+            textDecoration: 'underline',
+        },
+        button: {
+            display: 'block',
+            margin: '16px auto',
         },
     }),
 )
@@ -14,5 +30,13 @@ const useStyles = makeStyles((theme: Theme) =>
 export const SystemErrorPage: FC = () => {
     const classes = useStyles()
 
-    return <Card className={classes.root}>システムエラーが発生しました</Card>
+    return (
+        <>
+            <Box className={classes.root}>
+                <p className={classes.text}>システムエラーが発生しました</p>
+                <p className={classes.text}>ご迷惑おかけしますが、復旧まで少々お待ちください</p>
+            </Box>
+            <Footer />
+        </>
+    )
 }
